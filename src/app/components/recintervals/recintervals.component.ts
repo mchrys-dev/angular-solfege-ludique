fb9c75f9642cdc019dca9cb4893bdcfcb0302d87
mchrys-dev <mchrys-dev@gmail;com>
2022-05-26 16:05:04 +0200

    Ajout de la première page

---
+++ src/app/components/recintervals/recintervals.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recintervals',
+  templateUrl: './recintervals.component.html',
+  styleUrls: ['./recintervals.component.scss']
+})
+export class RecintervalsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
