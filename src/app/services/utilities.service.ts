bcfb0be64ae94b3b99230d87a18a70377bdcfb92
mchrys-dev <mchrys-dev@gmail;com>
2022-06-03 15:23:10 +0200

    Factorisation de certaines fonctions et transfert dans le service utilities

--- src/app/services/utilities.service.ts
+++ src/app/services/utilities.service.ts
@@ -12,4 +12,20 @@ export class UtilitiesService {
     num2 === 0 ? percentage = 0 : percentage = Math.round((num1 * 100) / num2);
     return percentage;
   }
+
+  public getRightAnsByLevel(rightAns: any[], levelId: number) {
+    return rightAns.filter(answer => answer.levelId === levelId);
+  }
+
+  public getQuestionsByLevel(questions: any[], levelId: number) {
+    return questions.filter(question => question.levelId === levelId);
+  }
+
+  public getStatsPercentageByLevel(rightAnswers: any[], questions: any[], levelId: number) {
+    return this.getPercentage(rightAnswers.length, questions.length);
+  }
+
+  public getProgressbarWidth(rightAnswers: any[], questions: any[], levelId: number) {
+    return 'width: ' + this.getStatsPercentageByLevel(rightAnswers, questions, levelId) + '%';
+  }
 }
