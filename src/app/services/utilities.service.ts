9d30b3ff40bca45c5e26739474aace26c165b1a1
mchrys-dev <mchrys-dev@gmail;com>
2022-06-04 21:30:57 +0200

    Factorisation de code dans la vue

--- src/app/services/utilities.service.ts
+++ src/app/services/utilities.service.ts
@@ -22,10 +22,29 @@ export class UtilitiesService {
   }
 
   public getStatsPercentageByLevel(rightAnswers: any[], questions: any[], levelId: number) {
-    return this.getPercentage(rightAnswers.length, questions.length);
+    return this.getPercentage(this.getRightAnsByLevel(rightAnswers, levelId).length, this.getQuestionsByLevel(questions, levelId).length);
   }
 
   public getProgressbarWidth(rightAnswers: any[], questions: any[], levelId: number) {
     return 'width: ' + this.getStatsPercentageByLevel(rightAnswers, questions, levelId) + '%';
+  }
+
+  public getProgressbarColorClass(rightAnswers: any[], questions: any[], levelId: number) {
+    let className = '';
+    let testingVar = null;
+    levelId === -1 ? testingVar = this.getPercentage(rightAnswers.length, questions.length) : testingVar = this.getStatsPercentageByLevel(rightAnswers, questions, levelId);
+
+    switch(true) {
+      case (testingVar < 20):
+        className = 'danger';
+        break;
+      case (testingVar >= 20 &&  testingVar < 70):
+        className = 'warning';
+        break;
+      case (testingVar >= 70):
+        className = 'success';
+        break;
+    }
+    return className;
   }
 }
