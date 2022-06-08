adef957005bad009f18775e36c42747c138edfd0
mchrys-dev <mchrys-dev@gmail;com>
2022-06-08 14:29:38 +0200

    Ajout des fichiers mp3

--- src/app/services/notes.service.ts
+++ src/app/services/notes.service.ts
@@ -10,6 +10,7 @@ export class NotesService {
       id: 0,
       name: '',
       musiqwikText: '=',
+      sound: '',
       isTonic: false
     },
     // Première octave
@@ -17,126 +18,147 @@ export class NotesService {
       id: 1,
       name: 'Do',
       musiqwikText: 'r',
+      sound: 'C4',
       isTonic: true
     },
     {
       id: 3,
       name: 'Do#',
       musiqwikText: 'Òr',
+      sound: 'Csharp4',
       isTonic: true
     },
     {
       id: 4,
       name: 'Réb',
       musiqwikText: 'ãs',
+      sound: 'Db4',
       isTonic: true
     },
     {
       id: 6,
       name: 'Ré',
       musiqwikText: 's',
+      sound: 'D4',
       isTonic: true
     },
     {
       id: 8,
       name: 'Ré#',
       musiqwikText: 'Ós',
+      sound: 'Dsharp4',
       isTonic: true
     },
     {
       id: 9,
       name: 'Mib',
       musiqwikText: 'ät',
+      sound: 'Eb4',
       isTonic: true
     },
     {
       id: 11,
       name: 'Mi',
       musiqwikText: 't',
+      sound: 'E4',
       isTonic: true
     },
     {
       id: 12,
       name: 'Fab',
       musiqwikText: 'åu',
+      sound: 'Fb4',
       isTonic: false
     },
     {
       id: 13,
       name: 'Mi#',
       musiqwikText: 'Ôt',
+      sound: 'Esharp4',
       isTonic: false
     },
     {
       id: 14,
       name: 'Fa',
       musiqwikText: 'u',
+      sound: 'F4',
       isTonic: true
     },
     {
       id: 16,
       name: 'Fa#',
       musiqwikText: 'Õu',
+      sound: 'Fsharp4',
       isTonic: true
     },
     {
       id: 17,
       name: 'Solb',
       musiqwikText: 'æv',
+      sound: 'Gb4',
       isTonic: true
     },
     {
       id: 19,
       name: 'Sol',
       musiqwikText: 'v',
+      sound: 'G4',
       isTonic: true
     },
     {
       id: 21,
       name: 'Sol#',
       musiqwikText: 'Öv',
+      sound: 'Gsharp4',
       isTonic: true
     },
     {
       id: 22,
       name: 'Lab',
       musiqwikText: 'çw',
+      sound: 'Ab4',
       isTonic: true
     },
     {
       id: 24,
       name: 'La',
       musiqwikText: 'w',
+      sound: 'A4',
       isTonic: true
     },
     {
       id: 26,
       name: 'La#',
       musiqwikText: '×w',
+      sound: 'Asharp4',
       isTonic: true
     },
     {
       id: 27,
       name: 'Sib',
       musiqwikText: 'èx',
+      sound: 'Bb4',
       isTonic: false
     },
     {
       id: 29,
       name: 'Si',
       musiqwikText: 'x',
+      sound: 'B4',
       isTonic: false
     },
     {
       id: 30,
       name: 'Dob',
       musiqwikText: 'éy',
+      sound: 'Cb5',
       isTonic: false
     },
     {
       id: 31,
       name: 'Si#',
       musiqwikText: 'Øx',
+      sound: 'Bsharp4',
       isTonic: false
     },
     // Deuxième octave
@@ -144,126 +166,147 @@ export class NotesService {
       id: 32,
       name: 'Do',
       musiqwikText: 'y',
+      sound: 'C5',
       isTonic: false
     },
     {
       id: 34,
       name: 'Do#',
       musiqwikText: 'Ùy',
+      sound: 'Csharp5',
       isTonic: false
     },
     {
       id: 35,
       name: 'Réb',
       musiqwikText: 'êz',
+      sound: 'Db5',
       isTonic: false
     },
     {
       id: 37,
       name: 'Ré',
       musiqwikText: 'z',
+      sound: 'D5',
       isTonic: false
     },
     {
       id: 39,
       name: 'Ré#',
       musiqwikText: 'Úz',
+      sound: 'Dsharp5',
       isTonic: false
     },
     {
       id: 40,
       name: 'Mib',
       musiqwikText: 'ë{',
+      sound: 'Eb5',
       isTonic: false
     },
     {
       id: 42,
       name: 'Mi',
       musiqwikText: '{',
+      sound: 'E5',
       isTonic: false
     },
     {
       id: 43,
       name: 'Fab',
       musiqwikText: 'ì|',
+      sound: 'Fb5',
       isTonic: false
     },
     {
       id: 44,
       name: 'Mi#',
       musiqwikText: 'Û{',
+      sound: 'Esharp5',
       isTonic: false
     },
     {
       id: 45,
       name: 'Fa',
       musiqwikText: '|',
+      sound: 'F5',
       isTonic: false
     },
     {
       id: 47,
       name: 'Fa#',
       musiqwikText: 'Ü|',
+      sound: 'Fsharp5',
       isTonic: false
     },
     {
       id: 48,
       name: 'Solb',
       musiqwikText: 'í}',
+      sound: 'Gb5',
       isTonic: false
     },
     {
       id: 50,
       name: 'Sol',
       musiqwikText: '}',
+      sound: 'G5',
       isTonic: false
     },
     {
       id: 52,
       name: 'Sol#',
       musiqwikText: 'Ý}',
+      sound: 'Gsharp5',
       isTonic: false
     },
     {
       id: 53,
       name: 'Lab',
       musiqwikText: '~',
+      sound: 'Ab5',
       isTonic: false
     },
     {
       id: 55,
       name: 'La',
       musiqwikText: 'î~',
+      sound: 'A5',
       isTonic: false
     },
     {
       id: 57,
       name: 'La#',
       musiqwikText: 'Þ~',
+      sound: 'Asharp5',
       isTonic: false
     },
     {
       id: 58,
       name: 'Sib',
       musiqwikText: 'Þy',
+      sound: 'Bb5',
       isTonic: false
     },
     {
       id: 60,
       name: 'Si',
       musiqwikText: '&#732;',
+      sound: 'B5',
       isTonic: false
     },
     {
       id: 61,
       name: 'Dob',
       musiqwikText: 'y',
+      sound: 'Cb6',
       isTonic: false
     },
     {
       id: 62,
       name: 'Si#',
       musiqwikText: 'y',
+      sound: 'Bsharp5',
       isTonic: false
     }
   ];
--- src/app/services/webaudio.service.ts
+++ src/app/services/webaudio.service.ts
@@ -5,13 +5,56 @@ import { Injectable } from '@angular/core';
 })
 export class WebaudioService {
 
-  // C: any = null;
+  C4: any = null;
+  Csharp4: any = null;
+  Db4: any = null;
+  D4: any = null;
+  Dsharp4: any = null;
+  Eb4: any = null;
+  E4: any = null;
+  Fb4: any = null;
+  Esharp4: any = null;
+  F4: any = null;
+  Fsharp4: any = null;
+  Gb4: any = null;
+  Gsharp4: any = null;
+  Ab4: any = null;
+  A4: any = null;
+  Asharp4: any = null;
+  Bb4: any = null;
+  B4: any = null;
+  Cb5: any = null;
+  Bsharp4: any = null;
+
+  C5: any = null;
+  Csharp5: any = null;
+  Db5: any = null;
+  D5: any = null;
+  Dsharp5: any = null;
+  Eb5: any = null;
+  E5: any = null;
+  Fb5: any = null;
+  Esharp5: any = null;
+  F5: any = null;
+  Fsharp5: any = null;
+  Gb5: any = null;
+  Gsharp5: any = null;
+  Ab5: any = null;
+  A5: any = null;
+  Asharp5: any = null;
+  Bb5: any = null;
+  B5: any = null;
+  Cb6: any = null;
+  Bsharp5: any = null;
 
   sounds: any[] = [];
   buffer: any = null;
   audioCtx = new AudioContext();
 
-  sources: any[] = [];
+  sources: any[] = [
+    'C4', 'Csharp4', 'Db4', 'D4', 'Dsharp4', 'Eb4', 'E4', 'Fb4', 'Esharp4', 'F', 'Fsharp4', 'Gb4', 'G4', 'Gsharp4', 'Ab4', 'A4', 'Asharp4', 'Bb4', 'B4', 'Cb5', 'Bsharp4',
+    'C5', 'Csharp5', 'Db5', 'D5', 'Dsharp5', 'Eb5', 'E5', 'Fb5', 'Esharp5', 'F', 'Fsharp5', 'Gb5', 'G5', 'Gsharp5', 'Ab5', 'A5', 'Asharp5', 'Bb5', 'B5', 'Cb5', 'Bsharp5'
+  ];
 
   constructor() { 
     this.sounds.forEach(function(this: any, soundName: any) {
@@ -21,7 +64,7 @@ export class WebaudioService {
 
   loadSound(this: any, audioCtx: any, buffer: any, bufferName: any) {
     const request = new XMLHttpRequest();
-    request.open("GET", "assets/sounds/" + bufferName + ".wav");
+    request.open("GET", "assets/sounds/" + bufferName + ".mp3");
     request.responseType = "arraybuffer";
     request.onload = function(this: any) {
       let undecodedAudio = request.response;
